b436d245a4b4a03fbc2900e3cbe37dfe6b076537
dibr <daniel.bratov@unicontsoft.com>
2025-11-06 12:01:52 +0200

    Add footer component

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 createRoot(document.getElementById('root')).render(
     <>
         <Header />
+
+        <Footer />
     </>
 )
