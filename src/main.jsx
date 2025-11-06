a65ee2d822a1c898813a7f5c445a90739c67856a
dibr <daniel.bratov@unicontsoft.com>
2025-11-06 12:20:07 +0200

    Add User list component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import UserList from './components/UserList'
 
 createRoot(document.getElementById('root')).render(
     <>
         <Header />
+
+        <UserList />
 
         <Footer />
     </>
