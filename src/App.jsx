b820bbfc8ff36fd965a55b6efb526aa2f0e962ab
dibr <daniel.bratov@unicontsoft.com>
2025-11-07 12:50:01 +0200

    Make new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Header from './components/Header'
 import UserList from './components/UserList'
 import Footer from './components/Footer'
+import Pagination from './components/Pagination'
+import Search from './components/Search'
 
 export default function App() {
     return (
@@ -8,7 +10,15 @@ export default function App() {
             <Header />
 
             <main className="main">
-                <UserList />
+                <section className="card users-container">
+                    <Search />
+
+                    <UserList />
+
+                    <button className="btn-add btn">Add new user</button>
+
+                    <Pagination />
+                </section>        
             </main>
             
             <Footer />
